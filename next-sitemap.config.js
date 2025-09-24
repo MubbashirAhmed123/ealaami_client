ba5079176edf217b28016fb7230c08142c0c324c
MOHAMMED MUBBASHIR AHMED <97329674+MubbashirAhmed123@users.noreply.github.com>
2025-09-24 23:17:38 +0530

    added meta data

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+/** @type {import('next-sitemap').IConfig} */
+const config = {
   siteUrl: "https://ealaami.in",
   generateRobotsTxt: true,
-  changefreq: "daily",
-  priority: 0.7,
+  outDir: "./public", 
 };
+
+module.exports = config;
