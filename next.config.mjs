dc01089d4df26a1af818450e2552f30ffe2e9643
MOHAMMED MUBBASHIR AHMED <97329674+MubbashirAhmed123@users.noreply.github.com>
2025-09-24 23:06:46 +0530

    added meta data

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      // redirect www to non-www (preserves protocol and path)
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.ealaami.in' }],
-        destination: 'https://ealaami.in/:path*',
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
