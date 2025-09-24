db59093c661ec0a997e60f28a111bc71238584b5
MOHAMMED MUBBASHIR AHMED <97329674+MubbashirAhmed123@users.noreply.github.com>
2025-09-24 22:56:56 +0530

    added site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: "https://ealaami.in",
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 0.7,
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          { type: 'host', value: 'www.ealaami.in' } // redirect www to non-www
+        ],
+        destination: 'https://ealaami.in/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          { type: 'host', value: 'ealaami.in' } // redirect HTTP to HTTPS
+        ],
+        destination: 'https://ealaami.in/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
